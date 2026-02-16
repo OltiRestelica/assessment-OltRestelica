ab99df9bd4485188d93baa45f0f49166c4320578
OltiRestelica <oltirestelica@gmail.com>
2026-02-16 21:57:34 +0100

    Video controllers finished

--- server/controllers/VideoController.js
+++ server/controllers/VideoController.js
@@ -41,4 +41,95 @@ const createVideo = (req, res) => {
     });
 };
 
-module.exports = { createVideo };
+const getAllVideos = (req, res) => {
+  Video.findAll({
+    include: [
+      {
+        model: User,
+        attributes: ["user_id", "name"],
+      },
+    ],
+  })
+    .then((videos) => {
+      res.status(200).json({
+        status: 1,
+        data: videos,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: 0,
+        data: err.message,
+      });
+    });
+};
+
+const getVideoById = (req, res) => {
+  const { id } = req.params;
+  Video.findByPk(id, {
+    include: [
+      {
+        model: User,
+        attributes: ["user_id", "name"],
+      },
+      {
+        model: Annotation,
+      },
+      {
+        model: Bookmark,
+      },
+    ],
+  })
+    .then((video) => {
+      if (!video) {
+        return res.status(404).json({
+          status: 0,
+          message: "Video not found",
+        });
+      }
+      res.status(200).json({
+        status: 1,
+        data: video,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: 0,
+        data: err.message,
+      });
+    });
+};
+
+const deleteVideo = (req, res) => {
+  const { id } = req.params;
+  Video.findByPk(id)
+    .then((video) => {
+      if (!video) {
+        return res.status(404).json({
+          status: 0,
+          message: "Video not found",
+        });
+      }
+      if (video.user_id !== req.user.user_id && req.user.role !== "Admin") {
+        return res.status(403).json({
+          status: 0,
+          message: "You don't have permission to delete this video",
+        });
+      }
+      return video.destroy();
+    })
+    .then(() => {
+      res.status(200).json({
+        status: 1,
+        message: "Video deleted",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: 0,
+        data: err.message,
+      });
+    });
+};
+
+module.exports = { createVideo, getAllVideos, getVideoById, deleteVideo };
--- server/routes/videoRoutes.js
+++ server/routes/videoRoutes.js
@@ -2,9 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middleware/auth");
+const isAdmin = require("../middleware/isAdmin");
 const upload = require("../middleware/upload");
-const { createVideo } = require("../controllers/VideoController");
+const {
+  createVideo,
+  getAllVideos,
+  getVideoById,
+  deleteVideo,
+} = require("../controllers/VideoController");
 
 router.post("/upload", authMiddleware, upload.single("video"), createVideo);
+router.get("/allVideos", authMiddleware, getAllVideos);
+router.get("/:id", authMiddleware, getVideoById);
+router.delete("/:id", authMiddleware, deleteVideo);
 
 module.exports = router;
