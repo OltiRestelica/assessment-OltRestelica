7986ea8efcd9c2d0656752e1eba1551eda6a7885
OltiRestelica <oltirestelica@gmail.com>
2026-02-17 18:30:29 +0100

    video template finished and homePage started

---
+++ client/src/components/VideoCard.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
+const VideoCard = ({ video }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="videoCard"
+      onClick={() => navigate(`/video/${video.video_id}`)}
+    >
+      <div className="thumbnail">
+        <video src={`http://localhost:3000${video.fileUrl}`}></video>
+      </div>
+      <div className="videoInfo">
+        <h3>{video.title}</h3>
+        <p>Uplaoded by: {video.User?.name}</p>
+        <p>{new Date(video.createdAt).toLocaleDateString()}</p>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,39 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../services/api";
+import VideoCard from "../components/VideoCard";
+import "../styles/HomePage.css";
+
 const HomePage = () => {
-  return <>hello</>;
+  const [videos, setVideos] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const res = await api.get("/video/allVideos");
+        setVideos(res.data.data);
+      } catch (error) {
+        console.error("Failed to fetch videos:", error);
+      }
+    };
+
+    fetchVideos();
+  }, []);
+  return (
+    <div className="homeContainer">
+      <div className="homeHeader">
+        <button className="uploadButton" onClick={() => navigate("/upload")}>
+          Upload Video
+        </button>
+      </div>
+      <div className="videoGrid">
+        {videos.map((video) => (
+          <VideoCard key={video.video_id} video={video} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
